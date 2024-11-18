6c2b290371afe8914b2986783b346e2bf97fbcb1
noel.sasikanth <noel.sasikanth@hilabs.com>
2024-11-18 22:08:27 +0530

    chat fixes

--- app/api/chat/list/route.js
+++ app/api/chat/list/route.js
@@ -1,17 +1,17 @@
 // pages/api/messages/last.js
 
-import prisma from "../../../lib/db"; // Assuming Prisma is set up
-import { NextResponse } from "next/server";
-import { getUserFromSession } from "../../../lib/currentSesion";
+import prisma from '../../../lib/db' // Assuming Prisma is set up
+import { NextResponse } from 'next/server'
+import { getUserFromSession } from '../../../lib/currentSesion'
 
 export async function GET(req) {
   try {
-    const currentUser = await getUserFromSession(req);
+    const currentUser = await getUserFromSession(req)
     if (!currentUser) {
       return NextResponse.json(
-        { message: "User not authenticated" },
+        { message: 'User not authenticated' },
         { status: 401 }
-      );
+      )
     }
 
     const conversations = await prisma.$queryRaw`
@@ -36,23 +36,26 @@ export async function GET(req) {
       ) AND m.created_at = latest.latest_message
       INNER JOIN User u ON u.id = latest.other_user_id
       ORDER BY m.created_at DESC
-    `;
+    `
 
-    const formattedConversations = conversations.map((conv) => ({
+    const formattedConversations = conversations.map(conv => ({
       id: conv.id.toString(),
-      other_user_id: conv.receiver_id.toString(),
+      other_user_id:
+        conv.sender_id === currentUser.id
+          ? conv.receiver_id.toString()
+          : conv.sender_id.toString(),
       username: conv.username,
       profilePic: conv.profilePic,
       last_message: conv.content,
       last_message_time: conv.created_at.toISOString(),
-    }));
+    }))
 
-    return NextResponse.json(formattedConversations);
+    return NextResponse.json(formattedConversations)
   } catch (error) {
-    console.error("Error fetching conversations:", error);
+    console.error('Error fetching conversations:', error)
     return NextResponse.json(
-      { message: "Error fetching conversations", error: error.message },
+      { message: 'Error fetching conversations', error: error.message },
       { status: 500 }
-    );
+    )
   }
 }
